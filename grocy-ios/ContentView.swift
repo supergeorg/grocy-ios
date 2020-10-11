5e6fc18ead4a46dcf40b30735bf7c56ac5d90a7b
Georg Meißner <Georg.Meissner@mailbox.tu-dresden.de>
2020-10-11 20:24:56 +0200

    Initial Commit

---
+++ grocy-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  grocy-ios
+//
+//  Created by Georg Meissner on 11.10.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ grocy-ios/grocy_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  grocy_iosApp.swift
+//  grocy-ios
+//
+//  Created by Georg Meissner on 11.10.20.
+//
+
+import SwiftUI
+
+@main
+struct grocy_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
